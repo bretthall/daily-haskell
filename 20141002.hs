385ea2f9eb4d9e682d8231d47a0f207596b0253a
Brett Hall <bretthall@speedymail.org>
2014-10-02 21:15:56 +0100

    Added some comments

--- 20141002.hs
+++ 20141002.hs
@@ -20,6 +20,11 @@ factors n = filter (\x -> n `mod` x == 0) [1..(n - 1)]
 
 sumEquals n xs = sum xs == n
 
+-- Works but for base > 5 it won't finish in a reasonable amount of time ("reasonable" being defined very loosely).
+-- The problem is that subsequences generates the smallest sequences first so we have to go through all of 
+-- them to get to the longest sequence. Instead we shoudl be starting with the longest and quitting when we find the first 
+-- valid sequence. Plus, if we find a sequence that sums to something less than what we're looking for we can ignore any 
+-- sequences that are generated from that sequence by removing elements.
 distinctFactorsSum :: Int -> [Int]
 distinctFactorsSum base = head $  filter (sumEquals f) $ sortBy (\x y -> compare (length y) (length x)) $ subsequences $ factors f
     where 
@@ -34,3 +39,5 @@ summer base sequence = product [1 .. base] == sum sequence
 -- summer x ans ~> True
 
 -- for this problem the (uninforced) range of base is in [3 .. 7]
+
+    
