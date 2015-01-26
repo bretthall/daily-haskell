a53dfa4dcfefe066899ca650acee6ba965896dac
Brett Hall <bretthall@speedymail.org>
2015-01-26 12:48:31 -0800

    More work on Harriss before moving the Harriss stuff into its own project

--- 20150114.hs
+++ 20150114.hs
@@ -91,16 +91,20 @@ results.
 --}
 
 data Rect = Rect {lowerLeft::Point, upperRight::Point} deriving Show
-data Division = Div {square::Rect, entry::Corner, rect1::Division, rect2::Division} deriving Show
+
+data Division = Div {entry::Corner, square::Rect, rect1::(Rect, Division), rect2::(Rect, Division)}
+
+instance Show Division where
+    show (Div c s (r1, _) (r2, _)) = "Div " ++ show c ++ " " ++ show s ++ " " ++ show r1 ++ " " ++ show r2
 
 p :: Double
 p = 1.32471795724474602596
-
-harrissDivisions :: Length -> Division
-harrissDivisions h = hDiv initRect (C Lower Left)
+                 
+divisions :: Length -> Division
+divisions h = div initRect (C Lower Left)
     where
-      initRect = Rect (P 0 0) (P h*p h)
-      hDiv (Rect (P x1 y1) (P x2 y2)) c@(C Lower Left) = Div sqr c r1 r2
+      initRect = Rect (P 0 0) (P (h*p) h)
+      div (Rect (P x1 y1) (P x2 y2)) c@(C Lower Left) = Div c sqr (r1, div r1 (C Lower Right)) (r2, div r2 c)
           where
             h = y2 - y1
             w = x2 - x1
@@ -108,9 +112,9 @@ harrissDivisions h = hDiv initRect (C Lower Left)
             s = h' * p
             w' = h / p
             sqr = Rect (P (x1 + w') y1) (P x2 (y1 + s)) 
-            r1 = vDiv (Rect (P x1 y1) (P (x1 + w') y2)) (C Lower Right)
-            r2 = hDiv (Rect (P (x1 + w') (y1 + s)) (P x2 y2)) (C Lower Left)
-      vDiv (Rect (P x1 y1) (P x2 y2)) c@(C Lower Right) = Div sqr c r1 r2
+            r1 = Rect (P x1 y1) (P (x1 + w') y2)
+            r2 = Rect (P (x1 + w') (y1 + s)) (P x2 y2)
+      div (Rect (P x1 y1) (P x2 y2)) c@(C Lower Right) = Div c sqr (r1, div r1 (C Upper Right)) (r2, div r2 c)
           where
             h = y2 - y1
             w = x2 - x1
@@ -118,19 +122,19 @@ harrissDivisions h = hDiv initRect (C Lower Left)
             s = w' * p
             h' = w / p
             sqr = Rect (P (x1 + w') (y1 + h')) (P x2 y2) 
-            r1 = hDiv (Rect (P x1 y1) (P x2 (y2 + h'))) (C Upper Right)
-            r2 = vDiv (Rect (P x1 (y1 + h')) (P (x1 + w') y2)) (C Lower Right)
-      hDiv (Rect (P x1 y1) (P x2 y2)) c@(C Upper Right) = Div sqr c r1 r2
+            r1 = Rect (P x1 y1) (P x2 (y2 + h'))
+            r2 = Rect (P x1 (y1 + h')) (P (x1 + w') y2)
+      div (Rect (P x1 y1) (P x2 y2)) c@(C Upper Right) = Div c sqr (r1, div r1 (C Upper Left)) (r2, div r2 c)
           where
             h = y2 - y1
             w = x2 - x1
             h' = h / (1.0 + p)
             s = h' * p
             w' = h / p
-            sqr = Rect (P x1 (y1 + h') (P (x1 + s) y2) 
-            r1 = vDiv (Rect (P (x1 + s) y1) (P x2 y2)) (C Upper Left)
-            r2 = hDiv (Rect (P x1 y1) (P (x1 + s) (y1 + h'))) (C Upper Right)
-      vDiv (Rect (P x1 y1) (P x2 y2)) c@(C Upper Left) = Div sqr c r1 r2
+            sqr = Rect (P x1 (y1 + h')) (P (x1 + s) y2)
+            r1 = Rect (P (x1 + s) y1) (P x2 y2)
+            r2 = Rect (P x1 y1) (P (x1 + s) (y1 + h'))
+      div (Rect (P x1 y1) (P x2 y2)) c@(C Upper Left) = Div c sqr (r1, div r1 (C Lower Left)) (r2, div r2 c)
           where
             h = y2 - y1
             w = x2 - x1
@@ -138,8 +142,16 @@ harrissDivisions h = hDiv initRect (C Lower Left)
             s = w' * p
             h' = w / p
             sqr = Rect (P x1 y1) (P (x1 + s) (y1 + s)) 
-            r1 = hDiv (Rect (P x1 (y1 + s)) (P x2 y2)) (C Lower Left)
-            r2 = vDiv (Rect (P (x1 + s) y1) (P x2 (y1 + s))) (C Upper Left)
+            r1 = Rect (P x1 (y1 + s)) (P x2 y2)
+            r2 = Rect (P (x1 + s) y1) (P x2 (y1 + s))
+
+generations :: Division -> [[Division]]
+generations d = [d]:(next [d])
+    where
+      next ds = ds':(next ds')
+          where 
+            ds' = concatMap (\d -> [(snd.rect1) d, (snd.rect2) d]) ds
+      
 
 harrissSpiral :: Dimensions -> [Point]
 harrissSpiral rectangle = undefined
