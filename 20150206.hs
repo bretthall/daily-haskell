3c93619c4e595c79a9dacea8e97909ce1a144d22
Brett Hall <bretthall@speedymail.org>
2015-02-06 10:01:46 -0800

    Figured out how to use bytestring for input

--- 20150206.hs
+++ 20150206.hs
@@ -10,15 +10,21 @@ http://lpaste.net/120086
 
 --}
 
-import Data.Text as T
+--Should probably figure out this new-fangled Data.Text business, now seems like as good a time as any,
+--plus there's no splitOn for regular strings
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import qualified Data.ByteString as B
 
 itsFriday :: FilePath -> IO Int
-itsFriday song = readFile song >>= return . countFridays
+itsFriday song = B.readFile song >>= return . countFridays . TE.decodeUtf8
 
-countFridays :: String -> Int
-countFridays = (\n -> n - 1) . Prelude.length . (splitOn $ T.pack "friday") . T.toLower . T.pack
+countFridays :: T.Text -> Int
+countFridays = (\n -> n - 1) . length . (T.splitOn $ T.pack "friday") . T.toLower
 
 {--
+After much time figuring out how to fix my "multiple bytestring's installed" woes we get:
+
 *Main> itsFriday "20150206-lyrics.txt"
 27
 --}
